fb976e92e1b0e2cdb0fe65601be3bcf201912e34
Hari Kolasani <hkolasani@bluecloudsystems.com>
2015-11-16 15:47:35 -0800

    CMIS

--- CaseBot.py
+++ CaseBot.py
@@ -1,6 +1,6 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from cmislib import CmisClient, Repository, Folder
-import httplib,sys
+import httplib,sys,json
 
 app = Flask(__name__)
 
@@ -10,15 +10,30 @@ theChannel = ''
 @app.route('/CaseBot', methods=['GET', 'POST'])
 def hello_world():
 
+    docs = []
+    postText = ""
+    responseURL = ""
+
     try:
       client = CmisClient('http://cmis.alfresco.com/cmisatom', 'admin', 'admin')
       repo = client.defaultRepository
       print repo.id
 
+      cmisdocs = repo.query("select * from cmis:document where cmis:name LIKE '%Hari%'")
+      for cmisdoc in cmisdocs:
+            print cmisdoc.name
+            doc = {}
+            doc['title'] = cmisdoc.name
+            docs.append(doc)
+            postText = postText + cmisdoc.name + '\n'
+
+      print jsonify(results=docs)
+      responseURL = request.values['response_url']
       channelName = request.values['channel_name']
       channelId = request.values['channel_id']
       token = request.values['token']
       userId = request.values['user_id']
+
     except:
       print "Unexpected error:", sys.exc_info()[0]
       e = sys.exc_info()[0]
@@ -27,16 +42,19 @@ def hello_world():
         if token != theToken:
             return '{"text":"Invalid Token in the Request"}'
         else :
-            postToSlack(channelName)
+            postToSlack(postText,responseURL)
     finally:
-        return '{"text":"RepoId from pyCharm to "' + repo.id + channelName + '"}!'
+        #return '{"text":"RepoId from pyCharm to "' + repo.id + channelName + '"}!'
+        print json.dumps(docs)
+        return jsonify(results=docs)
 
-def postToSlack(chnl):
+def postToSlack(bodyText,postURL):
 
-    #body = '{"text":"Posting from pyCharm' + chnl + '"}'
-    body = '{"text":"/testcmd"}'
+    #body = '{"text":"' + bodyText + '"}'
+    body = '{"text":"<http://www.google.com>"}'
     conn = httplib.HTTPSConnection("hooks.slack.com")
-    conn.request("POST", "/services/T0DMM2G9H/B0DQK99SM/5NWo2oxIn3l4SXoD2seyDBqu",body)
+    #conn.request("POST", "/services/T0DMM2G9H/B0DQK99SM/5NWo2oxIn3l4SXoD2seyDBqu",body)
+    conn.request("POST",postURL,body)
     response = conn.getresponse()
     conn.close()
     print response.status, response.reason

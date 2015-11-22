e59a9ef6a7d65a06167c4811a0388ca0e8f24e42
Hari Kolasani <hkolasani@bluecloudsystems.com>
2015-11-21 17:01:57 -0800

    CMIS

--- CaseBot.py
+++ CaseBot.py
@@ -5,53 +5,65 @@ import random
 
 app = Flask(__name__)
 
-theToken = 'sII3Kx1Kml2sJWgbcFyNCRlh'
+slackToken = 'sII3Kx1Kml2sJWgbcFyNCRlh'
 theChannel = ''
 
-@app.route('/CaseBot', methods=['GET', 'POST'])
-def hello_world():
-
-    attachmentsDict = {}
-    attachments = []
+@app.route('/CaseBot', methods=['POST'])
+def doPost():
 
     try:
-      client = CmisClient('http://cmis.alfresco.com/cmisatom', 'admin', 'admin')
-      repo = client.defaultRepository
-      print repo.id
+        token = request.values['token']
+        if token != slackToken:
+            return '{"text":"Error: Invalid Token!"}'
 
-      cmisdocs = repo.query("select * from cmis:document where cmis:name LIKE '%Hari%'")
+        cmdArg = request.values['text']
+        if cmdArg == None:
+            createNewCase(request)
+        else:
+            args = cmdArg.split(" ")
+            caseNumber = args[0]
+            action = args[1]
 
-      for cmisdoc in cmisdocs:
-            props = [{"title":"Author","value":"jhgjgj","short":True},{"title":"Date Created","value":"3 Days Ago","short":True}]
-            #attachment = {"title":cmisdoc.name,"text":cmisdoc.id,"title_link":cmisdoc.id,"color":generateColor(),"fields":props}
-            attachment = {"title":cmisdoc.name,"text":cmisdoc.id,"title_link":cmisdoc.id,"color":generateColor()}
-            attachments.append(attachment)
+            if caseNumber == None or action == None:
+                return '{"text":"Error: Invalid Command!"}'
 
-      attachmentsDict['attachments'] = attachments
-
-      #postResponse =  jsonify(results=attachmentsDict)
-      postText = json.dumps(attachments)
-
-      print postText
-
-      comamndArgument = request.values['text']
-
-      responseURL = request.values['response_url']    #this comes with the outgoing command
-      channelName = request.values['channel_name']
-      channelId = request.values['channel_id']
-      token = request.values['token']
-      userId = request.values['user_id']
-
+            if action == "docs":
+                getDocs(caseNumber)
+            elif action == "docs":
+                getDocs(caseNumber)
+            elif action == "docs":
+                getDocs(caseNumber)
+            elif action == "docs":
+                getDocs(caseNumber)
     except:
       print "Unexpected error:", sys.exc_info()[0]
       e = sys.exc_info()[0]
       return Response(status=500)
-    else:
-        if token != theToken:
-            return '{"text":"Invalid Token in the Request"}'
-        else :
-            postToSlack(postText,responseURL)
-            return Response(status=200)
+
+def getDocs(caseNumber):
+
+    attachmentsDict = {}
+    attachments = []
+
+    client = CmisClient('http://cmis.alfresco.com/cmisatom', 'admin', 'admin')
+    repo = client.defaultRepository
+
+    cmisdocs = repo.query("select * from cmis:document where cmis:name LIKE '%Hari%'")
+
+    for cmisdoc in cmisdocs:
+        props = [{"title":"Author","value":"jhgjgj","short":True},{"title":"Date Created","value":"3 Days Ago","short":True}]
+        #attachment = {"title":cmisdoc.name,"text":cmisdoc.id,"title_link":cmisdoc.id,"color":generateColor(),"fields":props}
+        attachment = {"title":cmisdoc.name,"text":cmisdoc.id,"title_link":cmisdoc.id,"color":generateColor()}
+        attachments.append(attachment)
+
+    attachmentsDict['attachments'] = attachments
+
+    postText = json.dumps(attachmentsDict)
+    responseURL = request.values['response_url']
+
+    postToSlack(postText,responseURL)
+
+    return Response(status=200)
 
 
 def postToSlack(bodyText,postURL):
@@ -63,17 +75,11 @@ def postToSlack(bodyText,postURL):
 
     salckcommandURI = postURL[len(salckcommandURL) +  1:len(postURL) - 1]
 
-    #body = '{"text":"<http://www.google.com>"}'
     conn = httplib.HTTPSConnection("hooks.slack.com")
-    #conn.request("POST", "/services/T0DMM2G9H/B0DQK99SM/5NWo2oxIn3l4SXoD2seyDBqu",body)  #to an incming web hook
 
     fullURL = salckcommandURL + '/' + salckcommandURI
 
-    #responsetype: inchannel is to post back to the channel instead of just the user
-    #body = '{"response_type": "in_channel","text":"' + bodyText + fullURL +  '"}'
-
-    #sending atatchements
-    body = '{"response_type": "in_channel","attachments":' + bodyText  + '}'
+    body = '{"response_type": "in_channel","' + bodyText  + '}'
 
     conn.request("POST",fullURL,body)
     response = conn.getresponse()
@@ -82,29 +88,7 @@ def postToSlack(bodyText,postURL):
 
     return
 
-def postToSlack1(bodyText,postURL):
-
-    salckcommandURL = 'https://hooks.slack.com/commands'
-
-    if postURL.index(salckcommandURL) < 0:
-        return
-
-    salckcommandURI = postURL[len(salckcommandURL) +  1:len(postURL) - 1]
-
-    #body = '{"text":"<http://www.google.com>"}'
-    conn = httplib.HTTPSConnection("hooks.slack.com")
-    #conn.request("POST", "/services/T0DMM2G9H/B0DQK99SM/5NWo2oxIn3l4SXoD2seyDBqu",body)  #to an incming web hook
-
-    fullURL = salckcommandURL + '/' + salckcommandURI
-
-    #responsetype: inchannel is to post back to the channel instead of just the user
-    body = '{"response_type": "in_channel","text":"' + bodyText + fullURL +  '"}'
-
-    conn.request("POST",fullURL,body)
-    response = conn.getresponse()
-    conn.close()
-    print response.status, response.reason
-
+def createNewCase(reqeust):
     return
 
 def generateColor():
